79cfda49129fb0f83e8942dae59ae66a62347fe8
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-01-29 21:40:55 +0100

    dealhand

--- bin/app.js
+++ bin/app.js
@@ -1,13 +1,15 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example');
 
 var PhaserGame = function () {
-    this.config;
+    this.config = null;
     this.players = [];
     this.players[0] = [];
     this.players[1] = [];
     this.combos = [];
     this.players[0].keys = [];
     this.players[1].keys = [];
+    this.players[0].hand = this.dealHand();
+    this.players[1].hand = this.dealHand();
     this.turn = 1;
 };
 
@@ -39,7 +41,6 @@ PhaserGame.prototype = {
     },
 
     update: function () {
-
         this.players.forEach(function(player) {
             player.keys.forEach(function(entry, i){
                 if (entry.justDown) {
@@ -49,10 +50,20 @@ PhaserGame.prototype = {
         }, this);
 
         if (this.players[0].chosenAttack != null && this.players[1].chosenAttack != null) {
-            console.log("Chosen attacks: " + this.players[0].chosenAttack + ' ' + this.players[1].chosenAttack);
+            console.log("Chosen attacks: " + this.players[0].hand[this.players[0].chosenAttack] + ' ' + this.players[1].hand[this.players[1].chosenAttack]);
             this.players[0].chosenAttack = null;
             this.players[1].chosenAttack = null;
+            console.log(this.players[0].hand);
+            console.log(this.players[1].hand);
         }
+    },
+    
+    dealHand: function () {
+        var result = [];
+        for(var i = 0;i < 4; i++) {
+            result[i] = Math.floor((Math.random() * 10) + 1);
+        }
+        return result;
     }
 };
 
