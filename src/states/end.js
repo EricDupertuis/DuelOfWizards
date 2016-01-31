5ae23d99c5903f6b9658139894bae5944797b5dd
Antoine Albertelli <antoinea101@gmail.com>
2016-01-31 12:03:01 +0100

    Implement fatalities (Satan still missing)

--- src/states/end.js
+++ src/states/end.js
@@ -4,26 +4,38 @@ var endState = function () {
 
 endState.prototype = {
     init: function() {
-        this.text = null;
     },
 
     preload: function() {
-
+        game.load.baseURL = 'assets/';
+        this.load.spritesheet('fatalities/Mayans', 'fatalities/Mayans.png', 600, 600);
+        this.load.spritesheet('fatalities/Satan', 'fatalities/Satan.png', 600, 600);
     },
 
     create: function() {
-        this.text= game.add.text(
-            game.world.centerX,
-            game.world.centerY,
-            'Winner',
-            { font: "65px Arial", fill: "#ff0044", align: "center" }
-        );
-        this.text.anchor.set(0.5);
+        var fatalities_name = 'fatalities/' + this.game.winner.faction;
+        console.log(fatalities_name);
 
-        this.game.add.tween(this.game.world).to( { alpha: 1 }, 500, "Linear", true );
+        this.game.add.tween(this.game.world)
+                     .to({ alpha: 1 }, 500, "Linear", true );
+
+        var sprite = game.add.image(game.world.width / 2,
+                                    game.world.height / 2,
+                                    fatalities_name);
+        sprite.anchor.setTo(0.5, 0.5);
+        var anim = sprite.animations.add(fatalities_name);
+        sprite.animations.play(fatalities_name, 5);;
+
+        anim.onComplete.add(function (sprite, animation) {
+            this.fadeExit = this.game.add.tween(this.game.world)
+                                          .to( { alpha: 0 }, 500, "Linear", true );
+            this.fadeExit.onComplete.add(function(){
+                game.state.start("Menu");
+            }, this);
+            sprite.destroy();
+        }, this);
     },
 
     update: function() {
-        this.text.text = this.game.winner.faction + " wins!"
     }
 };
